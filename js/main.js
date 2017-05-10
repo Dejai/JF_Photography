b6314b80f037777675ace61ec4106a8293390a74
Dejai <dlfyfield@smcm.edu>
2017-05-10 00:39:15 -0400

    Aded a modal for viewing individual images in the portfolio listing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
         var current = window.location.href;
         var slideIndex = 0;
-        var slideshow_image; 
+        var slideshow_images; 
         var myPositions;
         var theTimer;
         $.get(current+"config/slideshowImages.txt", function(results){
-            var all = results.split("\n");
-            for (var x in all){
-                var img = "<img class=\"slideshow_image\" src=\""+current+"images/slideshow/"+all[x]+"\">";
+            var slideshow_images_array = results.split("\n");
+            for (var x = 1; x < slideshow_images_array.length; x++){
+                var img = "<img class=\"slideshow_image\" src=\""+current+"images/slideshow/"+slideshow_images_array[x]+"\">";
                 $("#slideshowImagesContainer").append(img);
                 var pos = "<p class=\"slideshow_position\" data-ron-slide=\"" +x+"\"></p>";
                 $("#slideshowPositionsSection").append(pos);
             }
-            slideshow_image = $(".slideshow_image").toArray();
+            slideshow_images = $(".slideshow_image").toArray();
         	myPositions = $(".slideshow_position").toArray();
 	        carousel();
 	        $(".slideshow_position").click(function(){
@@ -26,8 +26,8 @@ $(document).ready(function(){
         	$(".slideshow_image").hide();
         	$(".slideshow_position").removeClass("currentSlidePosition");
 		    slideIndex++;
-		    if (slideIndex > slideshow_image.length) { slideIndex = 1} 
-		    $(slideshow_image[slideIndex-1]).show(); 
+		    if (slideIndex > slideshow_images.length) { slideIndex = 1} 
+		    $(slideshow_images[slideIndex-1]).show(); 
 		    $(myPositions[slideIndex-1]).addClass("currentSlidePosition"); 
 		    theTimer = setTimeout(carousel, 7000);
 		}
@@ -36,7 +36,7 @@ $(document).ready(function(){
         	clearTimeout(theTimer);
 		    $(this).blur();
 		    if (slideIndex == 1){
-		    	slideIndex = slideshow_image.length-1;
+		    	slideIndex = slideshow_images.length-1;
 		    }  else { 
 		    	slideIndex -= 2; 
 		    }
@@ -57,7 +57,7 @@ $(document).ready(function(){
 		    	clearTimeout(theTimer);
 			    $(this).blur();
 			    if (slideIndex == 1){
-			    	slideIndex = slideshow_image.length-1;
+			    	slideIndex = slideshow_images.length-1;
 			    }  else { 
 			    	slideIndex -= 2; 
 			    }
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,29 +1,31 @@
 $(document).ready(function(){
 	var query = window.location.search;
     if (query){
-    	var query_album = query.slice(6);
-    	getAlbumPhotos(query_album);
+    	var query_folder_name = query.slice(6);
+    	getAlbumPhotos(query_folder_name);
     } else {
     	getAlbumCovers();
     }
 });
 
+// @desc -- Makes the ajax call to retrieve information about the different albums
 function getAlbumCovers(){
 	$.get("/JF_Photography/config/albumCovers.txt", function(results){
     	var album_covers_info = results.split("\n");
     	var album_covers_listing = ""; 
 
     	for (var x = 1; x < album_covers_info.length; x++){
-    		var album_name = album_covers_info[x].split(", ")[0];
-    		var album_cover_img = album_covers_info[x].split(", ")[1];
+    		var folder_name = album_covers_info[x].split(", ")[0];
+    		var album_name = album_covers_info[x].split(", ")[1];
+    		var album_cover_img = album_covers_info[x].split(", ")[2];
     		if (x == 1){
-    			album_covers_listing = album_covers_listing + buildAlbumCover(album_name, album_cover_img, 1);
+    			album_covers_listing = album_covers_listing + buildAlbumCover(folder_name, album_name, album_cover_img, 1);
     		} else if ( (x % 3) == 0 && x != album_covers_info.length-1){
-    			album_covers_listing = album_covers_listing + buildAlbumCover(album_name, album_cover_img, 2);
+    			album_covers_listing = album_covers_listing + buildAlbumCover(folder_name, album_name, album_cover_img, 2);
     		} else if ( x == album_covers_info.length-1){
-    			album_covers_listing = album_covers_listing + buildAlbumCover(album_name, album_cover_img, 3);
+    			album_covers_listing = album_covers_listing + buildAlbumCover(folder_name, album_name, album_cover_img, 3);
     		} else {
-    			album_covers_listing = album_covers_listing + buildAlbumCover(album_name, album_cover_img, 4);
+    			album_covers_listing = album_covers_listing + buildAlbumCover(folder_name, album_name, album_cover_img, 4);
     		}
     	}
     	$("#albumTable").append(album_covers_listing);
@@ -34,26 +36,28 @@ function getAlbumCovers(){
 	    });
     });
 }
-function buildAlbumCover(albumName, coverImg, type){
+// @desc -- Used to build each album cover and return the element
+// @params -- Name of the album, the image for the album, which number in the row is it (i.e. type)
+function buildAlbumCover(folderName, albumName, coverImg, type){
 	var open_td, close_td;
 	var innerOpen = "<div class=\"inner\">";
 	var innerClose = "</div>";
-	var img = "<img class='album_image' src='/JF_Photography/images/slideshow/" + coverImg + "'/>";
+	var img = "<img class='album_image' src='/JF_Photography/images/gallery/"+folderName+"/"+ coverImg + "'/>";
 	switch(type){
 		case 1:
-			open_td = "<div class=\"album_cover_tr\">\n\t<div data-album-id=\""+albumName+"\" class=\"album_cover_td\">"; 
+			open_td = "<div class=\"album_cover_tr\">\n\t<div data-album-id=\""+folderName+"\" class=\"album_cover_td\">"; 
 			close_td = "</div>";
 			break;
 		case 2:
-			open_td = "\n\t<div data-album-id=\""+albumName+"\" class=\"album_cover_td\">";
+			open_td = "\n\t<div data-album-id=\""+folderName+"\" class=\"album_cover_td\">";
 			close_td = "</div>\n</div>\n<div class=\"album_cover_tr\">";
 			break;
 		case 3:
-			open_td = "\n\t<div data-album-id=\""+albumName+"\" class=\"album_cover_td\">" ;
+			open_td = "\n\t<div data-album-id=\""+folderName+"\" class=\"album_cover_td\">" ;
 			close_td = "</div>\n</tr>";
 			break;
 		case 4:
-			open_td = "\n\t<div data-album-id=\""+albumName+"\" class=\"album_cover_td\">";
+			open_td = "\n\t<div data-album-id=\""+folderName+"\" class=\"album_cover_td\">";
 			close_td = "</div>";
 			break;
 		default:
@@ -63,54 +67,92 @@ function buildAlbumCover(albumName, coverImg, type){
 	return open_td  + img + innerOpen  + albumName + innerClose + close_td; 
 }
 
-
-function getAlbumPhotos(album){
-	$.get("/JF_Photography/config/"+album+".txt", function(results){
+// @desc -- Makes the ajax call to retrieve information about the different albums. 
+// 			Also sets some event listeners for the images and related buttons (i.e. directional buttons)
+// @params -- The name of the album; Used to get the photos from the correct config file
+function getAlbumPhotos(folderName){
+    var gallery_photos_array;
+	$.get("/JF_Photography/config/"+folderName+".txt", function(results){
     	var album_photos = results.split("\n");
     	var album_photos_listing = ""; 
 
     	for (var x = 1; x < album_photos.length; x++){
-    		var photo = album_photos[x]
+    		var photo = album_photos[x].split(", ")[0];
+    		var description = album_photos[x].split(", ")[1];
     		if (x == 1){
-    			// console.log("<tr>\n\t<td>" + albumCover + "</td>");
-    			album_photos_listing = album_photos_listing + buildAlbumPhoto(album, photo, 1);
+    			album_photos_listing = album_photos_listing + buildAlbumPhoto(folderName, photo, x, 1);
     		} else if ( (x % 3) == 0 && x != album_photos.length-1){
-    			// console.log("\n\t<td>" + albumCover + "</td>\n</tr>\n<tr>");
-    			album_photos_listing = album_photos_listing + buildAlbumPhoto(album, photo, 2);
+    			album_photos_listing = album_photos_listing + buildAlbumPhoto(folderName, photo, x, 2);
     		} else if ( x == album_photos.length-1){
-    			// console.log("\n\t<td>" + albumCover + "</td>\n</tr>");
-    			album_photos_listing = album_photos_listing + buildAlbumPhoto(album, photo, 3);
+    			album_photos_listing = album_photos_listing + buildAlbumPhoto(folderName, photo, x, 3);
     		} else {
-    			// console.log("\n\t<td>" + albumCover + "</td>");
-    			album_photos_listing = album_photos_listing + buildAlbumPhoto(album, photo, 4);
+    			album_photos_listing = album_photos_listing + buildAlbumPhoto(folderName, photo, x, 4);
     		}
+            $("#galleryModalImageContainer").append("<img class=\"galleryImage\" src='/JF_Photography/images/gallery/"+folderName+"/" + photo + "'/>");
+            $("#galleryModalImageDescriptions").append("<p class=\"galleryDescription\">" + description + "</p>");
+
     	}
     	$("#albumTable").append(album_photos_listing);
-    	// $(".album_photos_td").click(function(){
-	    //     var photoID = $(this).attr("data-photo-id").replace(" ", "").toLowerCase();
-	    //     var albumPath = window.location.href; 
-	    //     window.location.assign(albumPath + "albums.html?name="+albumID);
-	    // });
+    	$(".galleryImage").hide();
+    	$(".galleryDescription").hide();
+    	gallery_photos_array = $(".galleryImage").toArray();
+    	gallery_descriptions_array = $(".galleryDescription").toArray();
+    	$("#numberOfGalleryImages").html(gallery_photos_array.length);
+    	$(".album_photos_td").click(function(){
+    		$("#galleryModal").show().css("z-index", 10);
+    		$("body").css("overflow", "hidden");
+    		var photoID = Number($(this).attr("data-photo-id"));
+    		$("#currentGalleryImage").html(photoID);
+    		$(gallery_photos_array[photoID-1]).show();
+    		$(gallery_descriptions_array[photoID-1]).show();
+	    });
+	    $("#closeGalleryModal").click(function(){
+    		$("#galleryModal").css("z-index", -10).hide();
+    		$(".galleryImage").hide();
+    		$(".galleryDescription").hide();
+    		$("body").css("overflow", "scroll");
+
+	    });
+    });
+    $("#galleryModalLeftButton").click(function(){
+    	nextGalleryImage(gallery_photos_array, gallery_descriptions_array , "left");
+    });
+    $("#galleryModalRightButton").click(function(){
+    	nextGalleryImage(gallery_photos_array, gallery_descriptions_array , "right");
+    });
+    $(document).keydown(function(event){
+    	if(event.which == 37){
+    		nextGalleryImage(gallery_photos_array, gallery_descriptions_array , "left");
+    	} else if (event.which == 39){
+	    	nextGalleryImage(gallery_photos_array, gallery_descriptions_array , "right");
+    	} else if (event.which == 27){
+    		$("#galleryModal").css("z-index", -10).hide();
+    		$(".galleryImage").hide();
+    		$(".galleryDescription").hide();
+    		$("body").css("overflow", "scroll");
+    	}
     });
 }
-function buildAlbumPhoto(album, photo, type){
+// @desc -- Used to build each photo in the list for the album
+// @params -- Name of the album, the image for the album, the number in the loop, which number in the row is it (i.e. type)
+function buildAlbumPhoto(folderName, photo, photoID, type){
 	var open_td, close_td;
-	var img = "<img class='album_image' src='/JF_Photography/images/gallery/"+album+"/" + photo + "'/>";
+	var img = "<img class='album_image' src='/JF_Photography/images/gallery/"+folderName+"/" + photo + "'/>";
 	switch(type){
 		case 1:
-			open_td = "<div class=\"album_photos_tr\">\n\t<div data-photo-id=\""+album+"\" class=\"album_photos_td\">"; 
+			open_td = "<div class=\"album_photos_tr\">\n\t<div data-photo-id=\""+photoID+"\" class=\"album_photos_td\">"; 
 			close_td = "</div>";
 			break;
 		case 2:
-			open_td = "\n\t<div data-photo-id=\""+album+"\" class=\"album_photos_td\">";
+			open_td = "\n\t<div data-photo-id=\""+photoID+"\" class=\"album_photos_td\">";
 			close_td = "</div>\n</div>\n<div class=\"album_photos_tr\">";
 			break;
 		case 3:
-			open_td = "\n\t<div data-photo-id=\""+album+"\" class=\"album_photos_td\">" ;
+			open_td = "\n\t<div data-photo-id=\""+photoID+"\" class=\"album_photos_td\">" ;
 			close_td = "</div>\n</tr>";
 			break;
 		case 4:
-			open_td = "\n\t<div data-photo-id=\""+album+"\" class=\"album_photos_td\">";
+			open_td = "\n\t<div data-photo-id=\""+photoID+"\" class=\"album_photos_td\">";
 			close_td = "</div>";
 			break;
 		default:
@@ -119,3 +161,34 @@ function buildAlbumPhoto(album, photo, type){
 	}
 	return open_td  + img + close_td; 
 }
+
+// @desc -- Shows the next image in the gallery slideshow (accounts for direction)
+// @params -- The array of photos, the array of descriptions for the photos, the direction to go in the slideshow. 
+function nextGalleryImage(galleryPhotos, galleryDescriptions, direction){
+	var currentID = Number($("#currentGalleryImage").html());
+	var limitID = Number($("#numberOfGalleryImages").html())
+	var nextID; 
+	if (currentID == 1 && direction == "left" ){
+		nextID = limitID;
+		$(".galleryImage").hide();
+		$(".galleryDescription").hide();
+		$("#currentGalleryImage").empty().html(nextID);
+		$(galleryPhotos[nextID-1]).show();
+		$(galleryDescriptions[nextID-1]).show();
+	} else if (currentID == limitID && direction == "right" ){
+		nextID = 1;
+		$(".galleryImage").hide();
+		$(".galleryDescription").hide();
+		$("#currentGalleryImage").empty().html(nextID);
+		$(galleryPhotos[nextID-1]).show();
+		$(galleryDescriptions[nextID-1]).show();
+	} else {
+		nextID = direction == "right" ? currentID + 1 : currentID - 1;
+		$(".galleryImage").hide();
+		$(".galleryDescription").hide();
+		$("#currentGalleryImage").empty().html(nextID);
+		$(galleryPhotos[nextID-1]).show();
+		$(galleryDescriptions[nextID-1]).show();
+	}
+}
+
